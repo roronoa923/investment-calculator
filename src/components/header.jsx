4ad02da8e6407b81318df0b37aa2ab604965cebb
Mayank Thakur <mt2655273@gmail.com>
2026-03-27 14:19:34 +0530

    Create header component

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header id="header">
+        <img src="./investment-calculator-logo.png" alt="Bag with money" />
+        <h1>Investment-calculator</h1>
+        </header>
+    )
+}
